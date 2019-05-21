e0a6fbfacab27cd1cc03787967c77ff1ac2e852c
Rafapessoa <45376292+Rafapessoa@users.noreply.github.com>
2019-05-20 22:41:48 -0300

    Add files via upload
    Commit

---
+++ precache-manifest.567e20ef1d36136c37e757fe047639c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8199ea3b42cdcb24214960f0ad865f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52a603653651ea88b394",
+    "url": "/static/css/main.66ab19cc.chunk.css"
+  },
+  {
+    "revision": "b206d2c31f393e5e370d",
+    "url": "/static/js/2.ff334f8f.chunk.js"
+  },
+  {
+    "revision": "52a603653651ea88b394",
+    "url": "/static/js/main.2fb70407.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
