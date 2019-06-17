fa89183683d1b67c5e579d9d8c33409cb784c562
Rafapessoa <45376292+Rafapessoa@users.noreply.github.com>
2019-06-17 15:15:59 -0300

    Alteração da rota

--- react/src/routes.jsx
+++ react/src/routes.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
 
 import Search from './pages/Search' ;
 import Product from './pages/Product' ;
 
-const Routes = () => (
-    <Switch>
-        <Route
-            exact
-            path='/'
-            component={ Search }
-        />
-        <Route
-            exact
-            path='/product/:id'
-            component={ Product }
-        />
-        <Route
-            component={ () => (
-                <div>Page not found</div>
-            ) }
-        />
-    </Switch>
-);
+const Routes = () => {
+    return (
+    <BrowserRouter basename="/">
+        <Switch>
+            <Route exact path='/' component={ Search }/>
+            <Route exact path='/product/:id' component={ Product }/>
+            <Route component={ () => (
+                <div>Page not found</div> )} />
+        </Switch>
+    </BrowserRouter>
 
+    )
+}
 export default Routes;
